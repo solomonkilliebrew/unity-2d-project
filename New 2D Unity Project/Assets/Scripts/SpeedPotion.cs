9bdf14cb0a08d3ec4f8371d8474b5b405837b4a5
solomonkilliebrew <55810572+solomonkilliebrew@users.noreply.github.com>
2019-11-06 15:45:20 -0600

    complete
    i did it

--- New 2D Unity Project/Assets/Scripts/SpeedPotion.cs
+++ New 2D Unity Project/Assets/Scripts/SpeedPotion.cs
@@ -3,7 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets._2D;
 
-public class SpeedPotion : MonoBehaviour
+public class SpeedPotion : Collectible
 {
+    PlatformerCharacter2D _character;
 
+    private void Awake()
+    {
+        _character = FindObjectOfType<PlatformerCharacter2D>();
+    }
+    public override void HandleCollected()
+    {
+        base.HandleCollected();
+        _character.DoubleSpeed();
+
+    }
 }
--- New 2D Unity Project/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ New 2D Unity Project/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -73,13 +73,13 @@ namespace UnityStandardAssets._2D
             if (m_Grounded || m_AirControl)
             {
                 // Reduce the speed if crouching by the crouchSpeed multiplier
-                move = (crouch ? move*m_CrouchSpeed : move);
+                move = (crouch ? move * m_CrouchSpeed : move);
 
                 // The Speed animator parameter is set to the absolute value of the horizontal input.
                 m_Anim.SetFloat("Speed", Mathf.Abs(move));
 
                 // Move the character
-                m_Rigidbody2D.velocity = new Vector2(move*m_MaxSpeed, m_Rigidbody2D.velocity.y);
+                m_Rigidbody2D.velocity = new Vector2(move * m_MaxSpeed, m_Rigidbody2D.velocity.y);
 
                 // If the input is moving the player right and the player is facing left...
                 if (move > 0 && !m_FacingRight)
@@ -87,7 +87,7 @@ namespace UnityStandardAssets._2D
                     // ... flip the player.
                     Flip();
                 }
-                    // Otherwise if the input is moving the player left and the player is facing right...
+                // Otherwise if the input is moving the player left and the player is facing right...
                 else if (move < 0 && m_FacingRight)
                 {
                     // ... flip the player.
@@ -115,5 +115,13 @@ namespace UnityStandardAssets._2D
             theScale.x *= -1;
             transform.localScale = theScale;
         }
-    }
+
+        public void DoubleSpeed()
+        {
+            m_MaxSpeed = m_MaxSpeed * 2;
+        }
+} 
+
+
+
 }
